451fba51e8f5f83e2d077a928a6bc3e06dccd2a9
Manuel Garcia <38119349+mgarcia92@users.noreply.github.com>
2018-11-14 23:29:26 -0400

    Proyecto Formato TXT SAP
    Formato TXT SAP para pagina gubernamental

---
+++ WinTestTxtFormat/WinTestTxtFormat/FileSap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinTestTxtFormat
+{
+    public class FileSap
+    {
+        public string AgenteRetencion { get; set; }
+        public string RifAgenteRetencion { get; set; }
+        public string PeriodoImpositivoAno { get; set; }
+        public string PeriodoImpositivoMes { get; set; }
+        public string RifSujetoDetenido { get; set; }
+        public string TipoOperacion { get; set; }
+        public string FechaFactura { get; set; }
+        public string NumeroFactura { get; set; }
+        public string NumeroControlFactura { get; set; }
+        public string TipoDocumento { get; set; }
+        public string CodigoContribuyente { get; set; }
+        public string NumeroFacturaAfectada { get; set; }
+        public string NumeroControlFacturaAfectada { get; set; }
+        public Int64 NumeroComprobante { get; set; }
+        public string BaseImponible { get; set; }
+        public string Alicuota { get; set; }
+        public string Codigo { get; set; }
+        public string ImpuestoRetenido { get; set; }
+        public string RUC { get; set; }
+   
+        public string getFileLine()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}\t{13}\t{14}\t{15}\t{16}\t{17}\t{18}", AgenteRetencion, RifAgenteRetencion,PeriodoImpositivoAno,PeriodoImpositivoMes,RifSujetoDetenido,TipoOperacion,FechaFactura,NumeroFactura,NumeroControlFactura,TipoDocumento,CodigoContribuyente,NumeroFacturaAfectada,NumeroControlFacturaAfectada,NumeroComprobante,BaseImponible,Alicuota,Codigo,ImpuestoRetenido,RUC);
+        }
+    }
+}
---
+++ WinTestTxtFormat/WinTestTxtFormat/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTestTxtFormat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(this.RutaArchivoTxt.Text))
+                {
+                    MessageBox.Show("Debe Seleccionar una ruta donde guardar el archivo para continuar!",
+                                   "Alerta",
+                                   MessageBoxButtons.OK,
+                                   MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!string.IsNullOrEmpty(this.ArchivoTxt.Text))
+                {
+                    char[] separator = new char[] { '\t' };
+                    var file = File.ReadAllLines(this.ArchivoTxt.Text, Encoding.UTF8);
+                    List<FileSap> data = new List<FileSap>();
+                    foreach (var item in file)
+                    {
+                        string[] lines = item.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (string.IsNullOrEmpty(lines[0]) && string.IsNullOrEmpty(lines[1]))
+                        {
+                            continue;
+                        }
+
+                        FileSap fileSap = new FileSap();
+                        fileSap.AgenteRetencion = lines[0].ToString();
+                        fileSap.RifAgenteRetencion = lines[1];
+                        fileSap.PeriodoImpositivoAno = lines[2];
+                        fileSap.PeriodoImpositivoMes = lines[3];
+                        fileSap.RifSujetoDetenido = lines[4];
+                        fileSap.TipoOperacion = lines[5];
+                        fileSap.FechaFactura = lines[6];
+                        fileSap.NumeroFactura = lines[7];
+                        fileSap.NumeroControlFactura = lines[8];
+                        fileSap.TipoDocumento = lines[9].ToString();
+                        fileSap.CodigoContribuyente = lines[10];
+                        fileSap.NumeroFacturaAfectada = lines[11];
+                        fileSap.NumeroControlFacturaAfectada = lines[12];
+                        fileSap.NumeroComprobante = Convert.ToInt64(lines[13]);
+                        fileSap.BaseImponible = lines[14];
+                        fileSap.Alicuota = lines[15];
+                        fileSap.Codigo = lines[16];
+                        fileSap.ImpuestoRetenido = lines[17];
+                        fileSap.RUC = lines[18];
+
+                        data.Add(fileSap);
+                    }
+                    string finalPath = $"{RutaArchivoTxt.Text}\\FileConverter_SAP.txt";
+                    using (FileStream fs = new FileStream(finalPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    {
+                        StringBuilder st = new StringBuilder();
+                        foreach (var item in data)
+                        {
+                            st.AppendLine(item.getFileLine());
+                        }
+                        byte[] arreglo = Encoding.UTF8.GetBytes(st.ToString());
+                        fs.Write(arreglo, 0, arreglo.Length);
+                        fs.Close();
+
+                    }
+
+                    MessageBox.Show("Archivo Convertido Con Exito");
+
+                }
+                else
+                {
+                    MessageBox.Show("Seleccione un Archivo para continuar");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrieron Errores => {ex.Message}",
+                                    "Error",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Stop);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            this.ArchivoTxt.Text = path;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.ShowDialog();
+            //string path = openFileDialog1.InitialDirectory;
+            //RutaArchivoTxt.Text = path;
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    //string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    RutaArchivoTxt.Text = fbd.SelectedPath;
+                    ///MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+    }
+}
